a10333d4b4920a05fde96f68db1abd99e041df43
Jouini Med youssef <168489700+MohamedYoussefjo@users.noreply.github.com>
2026-06-11 02:10:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Setup IfSecurity : édite les variables ci-dessous puis lance ./setup.sh
 
-# === 1. Variables à éditer ===
+#  1. Variables à éditer 
 
 SSH_PUBLIC_KEY="met-ta-clé-publique"
 TOTP_SECRET="JBSWY3DPEHPK3PXP"
@@ -9,13 +9,13 @@ RECAPTCHA_SITE_KEY="6Ld0aPwsAAAAAEHsFPzolGiecXHEYulyW3HNKZuT"
 RECAPTCHA_SECRET_KEY="6Ld0aPwsAAAAAP4b6iLliO9J4s3O9168EW-CkoST"
 
 
-# === 2. Injection de la clé publique SSH ===
+# 2. Injection de la clé publique SSH 
 
 echo "$SSH_PUBLIC_KEY" > ssh/authorized_keys
 chmod 644 ssh/authorized_keys
 
 
-# === 3. Écriture du secret TOTP ===
+#  3. Écriture du secret TOTP 
 
 echo "$TOTP_SECRET" > ssh/google_authenticator
 echo '" RATE_LIMIT 3 30' >> ssh/google_authenticator
@@ -25,31 +25,31 @@ echo '" TOTP_AUTH' >> ssh/google_authenticator
 chmod 644 ssh/google_authenticator
 
 
-# === 4. Mise à jour de login-api/app.py ===
+#  4. Mise à jour de login-api/app.py
 
 sed -i "s|MFA_SECRET = \".*\"|MFA_SECRET = \"$TOTP_SECRET\"|" login-api/app.py
 sed -i "s|RECAPTCHA_SECRET = \".*\"|RECAPTCHA_SECRET = \"$RECAPTCHA_SECRET_KEY\"|" login-api/app.py
 
 
-# === 5. Mise à jour de front-end/login.html ===
+#  5. Mise à jour de front-end/login.html 
 
 sed -i "s|data-sitekey=\".*\"|data-sitekey=\"$RECAPTCHA_SITE_KEY\"|" front-end/login.html
 
 
 
-# === 6. Permissions sur les logs ===
+# 6.a Permissions sur les logs 
 
 sudo chmod -R 777 ./logs
 
-# B. Restreint les permissions
+# 6.b  Restreindre les permissions sur .env
 sudo chmod 600 .env
 
-# === 7. Démarrage des conteneurs ===
+# 7. Démarrage des conteneurs 
 
 docker compose up -d --build
 
 
-# === 8. Import des dashboards Kibana ===
+# 8. Import des dashboards Kibana 
 
 # On attend que Kibana réponde
 sleep 30
@@ -59,7 +59,7 @@ curl -X POST http://localhost:5601/api/saved_objects/_import \
      --form file=@kibana/exports.ndjson
 
 
-# === 9. Récap ===
+# 9. Résumé et interfaces
 
 echo ""
 echo "Setup terminé."
